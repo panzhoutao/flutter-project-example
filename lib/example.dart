aaabf9d4048f8bed1ea6c2031bbe82e05c7561ce
taotao147 <836618033@qq.com>
2022-02-21 13:52:12 +0800

    1、init

--- lib/example.dart
+++ lib/example.dart
@@ -4,13 +4,13 @@
 library example;
 
 export 'package:flutter/material.dart';
-export 'package:flutter_project_example/generated/l10n.dart';
-export 'package:flutter_project_example/widget/base_state.dart';
-export 'package:flutter_project_example/pages/router_manage.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:flutter_utils_code/flutter_utils_code.dart';
 export 'package:provider/provider.dart';
-export 'package:flutter_project_example/common/net/api.dart';
-export 'package:flutter_project_example/utils/toast_utils.dart';
-export 'package:flutter_project_example/utils/log_util.dart';
-import 'generated/l10n.dart';
+
+export 'generated/l10n.dart';
+export 'widget/base_state.dart';
+export 'pages/router_manage.dart';
+export 'common/net/api.dart';
+export 'utils/toast_utils.dart';
+export 'utils/log_util.dart';
